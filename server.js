6b204fd9ed568d0f97c7200470e9492e5e986c4b
AdrianSK <56803219+AdrianSK75@users.noreply.github.com>
2022-06-18 12:16:43 +0300

    middleware

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+const logger = require("logger");
 const mongoose = require("mongoose")
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
       .then(() => app.listen(port, () => {console.log(`Starting the server with port: ${port}`)}))
       .catch((err) => console.log(err));
-
 app.use(express.json())
+app.use(logger("dev"))
+app.use(bodyParser.json())
 
 const tokensRouter = require("./routes/tokens")
 app.use("/tokens", tokensRouter);
