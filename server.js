cc2c2a760c5a6be020f4b1e7de4b7828de0a6771
AdrianSK <56803219+AdrianSK75@users.noreply.github.com>
2022-06-18 12:05:24 +0300

    Port Changed

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose")
 
-const dbURL = "mongodb+srv://Rares:parola12345@cluster0.jxvkaa4.mongodb.net/crypto?retryWrites=true&w=majority"
-mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
       .then(() => app.listen(port, () => {console.log(`Starting the server with port: ${port}`)}))
       .catch((err) => console.log(err));
 
